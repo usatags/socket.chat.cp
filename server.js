29a5ab0cb41f830b673be0974c1b68a0ff2d6190
sonnned <alejo1garciasosa@gmail.com>
2024-03-23 18:17:47 -0500

    Fix bug in login functionality

--- server.js
+++ server.js
@@ -486,33 +486,33 @@ io.on('connection', (socket) => {
               }
             })
 
-            const newMessage = await prisma.message.create({
-              data: {
-                content: `Invalid option. Please try again. You have ${3 - findByConversationID.failedTries} tries left.`,
-                sender_id: noSender[0].id,
-                conversation_id,
-                content_type: "text/auto/plates"
-              },
-              include: {
-                sender: {
-                  select: {
-                    id: true,
-                    username: true,
-                  }
-                }
-              }
-            })
+            // const newMessage = await prisma.message.create({
+            //   data: {
+            //     content: `Invalid option. Please try again. You have ${3 - findByConversationID.failedTries} tries left.`,
+            //     sender_id: noSender[0].id,
+            //     conversation_id,
+            //     content_type: "text/auto/plates"
+            //   },
+            //   include: {
+            //     sender: {
+            //       select: {
+            //         id: true,
+            //         username: true,
+            //       }
+            //     }
+            //   }
+            // })
 
-            io.to(conversation_id).emit('message', {
-              data: newMessage,
-            })
+            // io.to(conversation_id).emit('message', {
+            //   data: newMessage,
+            // })
 
-            io.emit(`notification-${noSender[0].id}`, {
-              title: 'New message',
-              body: `
-                  ${sender[0].username} has sent a new message
-                  `
-            })
+            // io.emit(`notification-${noSender[0].id}`, {
+            //   title: 'New message',
+            //   body: `
+            //       ${sender[0].username} has sent a new message
+            //       `
+            // })
           }
         } else if (!findByConversationID.state?.length) {
 
@@ -3388,8 +3388,6 @@ app.get('/room/:roomID/messages', async (req, res) => {
         },
       })
 
-      console.log('New room', newRoom)
-
       return res.status(200).json({
         data: newRoom,
         message: 'Messages fetched successfully',
@@ -3514,9 +3512,10 @@ app.get('/products', async (req, res) => {
 app.get('/purchases', async (req, res) => {
   try {
     const purchases = await prisma.purchasevisitor.findMany()
+    const purchasesFromPurchase = await prisma.purchase.findMany()
 
     return res.status(200).json({
-      data: purchases,
+      data: purchases.concat(purchasesFromPurchase),
       message: 'Purchases fetched successfully',
       success: true
     })    
@@ -3657,6 +3656,7 @@ app.post("/createPurchase", async (req, res) => {
     //     wantToGetVehicleInsurance: vehicleInsurance === 'false' ? 'false' : wantToGetVehicleInsurance,
     //   }
     // })
+    console.log('req.body', req.body)
     const purchase = await prisma.purchasevisitor.create({
       data: {
         vin,
@@ -3856,6 +3856,7 @@ app.post("/createPurchase", async (req, res) => {
 
 const generateAccessToken = async () => {
   try {
+    console.log(!process.env.PAYPAL_CLIENT_ID || !process.env.PAYPAL_CLIENT_SECRET)
     if (!process.env.PAYPAL_CLIENT_ID || !process.env.PAYPAL_CLIENT_SECRET) {
       throw new Error("MISSING_API_CREDENTIALS");
     }
@@ -3886,6 +3887,7 @@ const generateAccessToken = async () => {
 const generateClientToken = async () => {
   const accessToken = await generateAccessToken();
   const url = `${base}/v1/identity/generate-token`;
+  console.log('accessToken', accessToken)
   const response = await fetch(url, {
     method: "POST",
     headers: {
@@ -4325,12 +4327,494 @@ app.get('/env', async (req, res) => {
   try {
     const env = process.env
     res.status(200).json({
-      data: env,
+      data: {
+        viteServerURL: process.env.SERVER_URL,
+        viteCloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        viteCloudinaryPreset: process.env.CLOUDINARY_CLOUD_PRESET,
+        viteRapidAPIKey: process.env.RAPID_API_KEY,
+        viteRapidAPIHost: process.env.RAPID_API_HOST,
+        viteRapidAPIBaseURL: process.env.RAPID_API_URL,
+        vitePayPalClientID: process.env.PAYPAL_CLIENT_ID,
+      },
       message: 'Environment variables fetched successfully',
       success: true
     })
   } catch (error) {
     console.log('Error from env', error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+app.post('/purchaseFromFormConversation', async (req, res) => {
+  const { purchaseOption, data, room } = req.body
+  try {
+    if (purchaseOption === "0") {
+      await prisma.purchase.create({
+        data: {
+          vin: '',
+          color: '',
+          email: '',
+          details: '',
+          continuePurchase: true,
+          state: '',
+          conversation_id: data.conversationID,
+          user_id: data.senderID,
+          completed: false,
+          id: uuidv4(),
+          options: '',
+          address: '',
+          city: '',
+          zip: '',
+          phone: '',
+          driverLicense: '',
+          vehicleInsurance: '',
+          failedTries: 0,
+          cancelled: false,
+          houseType: '',
+          lastName: '',
+          name: '',
+          hasVehicleInSurance: '',
+          wantToGetVehicleInsurance: '',
+          isTruck: '',
+          total: 0,
+          image: '',
+          vehicleType: '',
+          buyingType: '',
+          paypalPaymentId: '',
+          insuranceType: '',
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "2"
+        })
+      })
+    } else if (purchaseOption === "2") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      const stateImage = jsonData.states.find((s) => s.state === data).id + '-' + data +'.webp'
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          state: data,
+          image: stateImage,
+          options: jsonData.states.filter((s) => s.state === data)[0].plates
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "3",
+          state: data,
+          options: jsonData.states.filter((s) => s.state === data)[0].plates
+        })
+      })
+    } else if (purchaseOption === "3") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      const price = data.split(' ')[data.split(' ').length - 1].split('$')[0]
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          details: data,
+          total: parseInt(price)
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "4"
+        })
+      })
+    } else if (purchaseOption === "4") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      const purchasePrice = allPurchases.filter((c) => c.conversation_id === room)[0].total
+      const vehicleTypeFee = ["Truck (+20$ fee)", "Bus (+20$ fee)", "Trailer (+20$ fee)"].includes(data) ? 20 : 0
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          vehicleType: data,
+          total: purchasePrice + vehicleTypeFee,
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "5"
+        })
+      })
+    } else if (purchaseOption === "5") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          name: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "6"
+        })
+      })
+    } else if (purchaseOption === "6") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          lastName: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "7"
+        })
+      })
+    } else if (purchaseOption === "7") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          address: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "8"
+        })
+      })
+    } else if (purchaseOption === "8") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          city: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "9"
+        })
+      })
+    } else if (purchaseOption === "9") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          houseType: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "10"
+        })
+      })
+    } else if (purchaseOption === "10") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          zip: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "11"
+        })
+      })
+    } else if (purchaseOption === "11") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          phone: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "12"
+        })
+      })
+    } else if (purchaseOption === "12") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          vin: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "13"
+        })
+      })
+    } else if (purchaseOption === "13") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          color: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "14"
+        })
+      })
+    } else if (purchaseOption === "14") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          email: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "15"
+        })
+      })
+    } else if (purchaseOption === "15") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      console.log('purchaseID', purchaseID, 'data', data)
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          driverLicense: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "16"
+        })
+      })
+    } else if (purchaseOption === "16") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          hasVehicleInSurance: data
+        }
+      }).then((purchase) => {
+        if (data === "yes") {
+          return res.status(200).json({
+            data: true,
+            currentStep: "18"
+          })
+        } else {
+          return res.status(200).json({
+            data: true,
+            currentStep: "17"
+          })
+        }
+      })
+    } else if (purchaseOption === "17") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          wantToGetVehicleInsurance: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "19"
+        })
+      })
+    } else if (purchaseOption === "18") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          vehicleInsurance: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "19",
+          purchaseID: purchaseID
+        })
+      })
+    } else if (purchaseOption === "19") {
+      const allPurchases = await prisma.purchase.findMany()
+      const purchaseID = allPurchases.filter((c) => c.conversation_id === room)[0].id
+      await prisma.purchase.update({
+        where: {
+          id: purchaseID
+        },
+        data: {
+          total: data
+        }
+      }).then((purchase) => {
+        return res.status(200).json({
+          data: true,
+          currentStep: "20"
+        })
+      })
+    }
+  } catch (error) {
+    console.log('Error from purchaseFromFormConversation', error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+app.get('/purchase/conversation/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    if (id) {
+      const purchaseS = await prisma.purchase.findMany()
+      const filteredByConversationID = purchaseS.filter((c) => c.conversation_id === id)
+      if (!filteredByConversationID.length || filteredByConversationID[0].completed) {
+        res.status(200).json({
+          data: false
+        })
+      } else {
+        if (!filteredByConversationID[0].state) {
+          res.status(200).json({
+            data: true,
+            currentStep: "2"
+          })
+        } else if (!filteredByConversationID[0].details) {
+          res.status(200).json({
+            data: true,
+            currentStep: "3",
+            state: filteredByConversationID[0].state,
+            options: jsonData.states.filter((s) => s.state === filteredByConversationID[0].state)[0].plates
+          })
+        } else if (!filteredByConversationID[0].vehicleType) {
+          res.status(200).json({
+            data: true,
+            currentStep: "4"
+          })
+        } else if (!filteredByConversationID[0].name) {
+          res.status(200).json({
+            data: true,
+            currentStep: "5"
+          })
+        } else if (!filteredByConversationID[0].lastName) {
+          res.status(200).json({
+            data: true,
+            currentStep: "6"
+          })
+        } else if (!filteredByConversationID[0].address) {
+          res.status(200).json({
+            data: true,
+            currentStep: "7"
+          })
+        } else if (!filteredByConversationID[0].city) {
+          res.status(200).json({
+            data: true,
+            currentStep: "8"
+          })
+        } else if (!filteredByConversationID[0].houseType) {
+          res.status(200).json({
+            data: true,
+            currentStep: "9"
+          })
+        } else if (!filteredByConversationID[0].zip) {
+          res.status(200).json({
+            data: true,
+            currentStep: "10"
+          })
+        } else if (!filteredByConversationID[0].phone) {
+          res.status(200).json({
+            data: true,
+            currentStep: "11"
+          })
+        } else if (!filteredByConversationID[0].vin) {
+          res.status(200).json({
+            data: true,
+            currentStep: "12"
+          })
+        } else if (!filteredByConversationID[0].color) {
+          res.status(200).json({
+            data: true,
+            currentStep: "13"
+          })
+        } else if (!filteredByConversationID[0].email) {
+          res.status(200).json({
+            data: true,
+            currentStep: "14"
+          })
+        } else if (!filteredByConversationID[0].driverLicense) {
+          res.status(200).json({
+            data: true,
+            currentStep: "15"
+          })
+        } else if (!filteredByConversationID[0].hasVehicleInSurance) {
+          res.status(200).json({
+            data: true,
+            currentStep: "16"
+          })
+        } else if (!filteredByConversationID[0].wantToGetVehicleInsurance) {
+          res.status(200).json({
+            data: true,
+            currentStep: "17"
+          })
+        } else if ((!filteredByConversationID[0].vehicleInsurance && filteredByConversationID[0].hasVehicleInSurance === 'yes') || (filteredByConversationID[0].hasVehicleInSurance === 'no' && filteredByConversationID[0].wantToGetVehicleInsurance === 'yes')) {
+          res.status(200).json({
+            data: true,
+            currentStep: "18"
+          })
+        } else if (!filteredByConversationID[0].completed) {
+          const purchaseID = filteredByConversationID[0].id
+          res.status(200).json({
+            data: true,
+            currentStep: "19",
+            purchaseID: purchaseID
+          })
+        }
+      }
+    } else {
+      return res.status(404).json({ error: 'Id no provided' })
+    }
+  } catch (error) {
+    console.log("Error from purchase conversation", error)
     res.status(500).json({ error: 'Internal server error' })
   }
 })
