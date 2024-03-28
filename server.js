84f9d70004b3cc02f3ad8ec485919960eb677535
sonnned <alejo1garciasosa@gmail.com>
2024-03-27 22:54:08 -0500

    Update CORS origin to 'https://usatag.us'

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ const base = "https://www.paypal.com";
 const app = express()
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: 'https://usatag.us',
   credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Origin", 'https://usatag.us');
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
