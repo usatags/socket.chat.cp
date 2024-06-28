8dd2f97696e02cfc2535c26e4bb510e95c1353d2
sonnned <alejo1garciasosa@gmail.com>
2024-06-27 22:24:21 -0500

    chore: Update server.js to include limit for request body size

--- server.js
+++ server.js
@@ -34,7 +34,10 @@ app.use(cors({
   // origin: "*",
   credentials: true
 }))
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
 app.use(cookieParser())
 app.use(function(req, res, next) {
   const allowedOrigins = ['https://usadealerplates.us', 'https://usatag.us', 'https://systemdmvusa.com'];
@@ -3933,7 +3936,7 @@ app.post("/createPurchase", async (req, res) => {
   } = req.body
 
   try {
-    console.log('req.body', req.body)
+    // console.log('req.body', req.body)
     const purchase = await prisma.purchasevisitor.create({
       data: {
         vin,
