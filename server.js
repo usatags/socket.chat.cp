9828f33b8c476da26e3fe855f0543379be78453f
sonnned <alejo1garciasosa@gmail.com>
2024-11-28 12:25:21 -0500

    fix: Update purchase creation validation to check for state presence

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.post("/createPurchase", async (req, res) => {
 
   try {
     // console.log('req.body', req.body)
-    if (state.includes("New Jersey") && !isInsurance) {
+    if (state && !isInsurance) {
       if (!vehicleInsurance && !insuranceProvider) {
         return res.status(400).json({
           error: 'Missing vehicle insurance or insurance provider'
